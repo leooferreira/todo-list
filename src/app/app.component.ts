1b20b99fbb3e11bd01ab71aae59e120caed6a2ca
Leonardo Ferreira <leooferreira07@gmail.com>
2022-07-10 11:48:54 -0300

    inserindo função toggle na nossa lista.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = [];
+  public title: String = 'Minhas Tarefas';
+
+  constructor() {
+  this.todos.push(new Todo(1, 'Ir ao supermercado', false));
+  this.todos.push(new Todo(2, 'Passear com a Amora', true));
+  this.todos.push(new Todo(3, 'Lavar a louça', true));   
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if(index != -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false;
+  }
+
+ 
 }
